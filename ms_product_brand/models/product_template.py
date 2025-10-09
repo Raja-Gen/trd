638a4ae406e48ee6656302773f53a294b675ee5d
Akash <its.akash30122000@gmail.com>
2025-10-09 14:41:07 +0530

    [ADD] stock freezing logic

--- ms_product_brand/models/product_template.py
+++ ms_product_brand/models/product_template.py
@@ -12,3 +12,24 @@ class ProductTemplate(models.Model):
         tracking=True,
         help="Select the brand for this product.",
     )
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    l10n_ae_tax_report_counterpart_account = fields.Char(
+        string="Liabilities Account",
+        config_parameter="l10n_ae_tax_report.liabilities_account",
+        help="Account used for tax liabilities.",
+    )
+        
+    l10n_ae_tax_report_liabilities_account = fields.Char(
+        string="Counterpart Account",
+        config_parameter="l10n_ae_tax_report.counterpart_account",
+        help="Account used for counterpart entries.",
+    )
+
+    l10n_ae_tax_report_counterpart_account = fields.Many2one(
+    'account.account',
+    string="Tax Report Counterpart Account"
+    )
+
--- website_customization/models/__init__.py
+++ website_customization/models/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import product_template
 from . import sale_order
+from . import stock_freeze
+from . import sale_order_line
